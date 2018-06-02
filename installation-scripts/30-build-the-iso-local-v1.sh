a4577c4f8f5d351453a950dda6cf65effa4ce3f5
Erik Dubois <erik.dubois@gmail.com>
2018-06-02 17:09:53 +0200

    New build script

--- installation-scripts/30-build-the-iso-local-v1.sh
+++ installation-scripts/30-build-the-iso-local-v1.sh
@@ -16,7 +16,7 @@
 #Let us change the name"
 #First letter of desktop small
 desktop="plasma"
-githuburl="https://raw.githubusercontent.com/arcolinuxb/arco-plasma/master/archiso/packages.both"
+
 #build.sh
 oldname1="iso_name=arcolinux"
 newname1="iso_name=arcolinuxb-$desktop"
@@ -37,53 +37,53 @@ newname5='DISTRIB_ID=ArcoLinuxB-'$desktop
 
 oldname6='DISTRIB_DESCRIPTION="ArcoLinux"'
 newname6='DISTRIB_DESCRIPTION=ArcoLinuxB-'$desktop
-
+echo
+echo "################################################################## "
 echo "Phase 1 : clean up and download the latest ArcoLinux-iso from github"
 echo "################################################################## "
-echo "Deleting the tmp folder if one exists"
-[ -d /tmp/git-clone-arcolinux-iso ] && sudo rm -rf /tmp/git-clone-arcolinux-iso
+echo
+echo "Deleting the work folder if one exists"
+[ -d ../work ] && rm -rf ../work
 echo "Deleting the build folder if one exists - takes some time"
 [ -d ~/arcolinuxb-build ] && sudo rm -rf ~/arcolinuxb-build
-echo "Git cloning files and folder to /tmp/arcolinuxb-build"
-git clone https://github.com/arcolinux/arcolinux-iso /tmp/git-clone-arcolinux-iso
-echo "Phase 2 : Getting the latest updates for some important files"
-echo "################################################################## "
-echo "Moving to the tmp folder to work"
-cd /tmp/git-clone-arcolinux-iso/installation-scripts/
-echo "Removing old files/folders from folder"
-rm -rf ../archiso/airootfs/etc/skel/.* 2> /dev/null
-echo "getting .bashrc from arcolinux-root"
-wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/root/.bashrc-latest -O ../archiso/airootfs/etc/skel/.bashrc
-echo ".bashrc copied to /etc/skel"
-echo
-#echo "getting oblogout.conf from arcolinux-oblogout-themes"
-#wget https://raw.githubusercontent.com/arcolinux/arcolinux-oblogout-themes/master/oblogout.conf.arcolinuxnew -O ../archiso/airootfs/etc/oblogout.conf
-#echo "oblogout.conf copied"
-#echo
-#echo "getting slim.conf from arcolinux-slimlock-themes"
-#wget https://raw.githubusercontent.com/arcolinux/arcolinux-slimlock-themes/master/slim.conf.arcolinuxnew -O ../archiso/airootfs/etc/slim.conf
-#echo "slim.conf copied"
-#echo
-echo "BYOI - getting the files for this specific desktop"
-rm ../archiso/packages.both
-wget $githuburl -O ../archiso/packages.both
-echo "Packages.both copied"
-echo "################################################################## "
+echo "Git cloning files and folder to work folder"
+git clone https://github.com/arcolinux/arcolinux-iso ../work
 
+echo
+echo "################################################################## "
+echo "Phase 2 : Getting the latest versions for some important files"
+echo "################################################################## "
+echo
+echo "Removing the old packages.both file from work folder"
+rm ../work/archiso/packages.both
+echo "Copying the new packages.both file"
+cp -f ../archiso/packages.both ../work/archiso/packages.both
+
+echo "Removing old files/folders from /etc/skel"
+rm -rf ../work/archiso/airootfs/etc/skel/.* 2> /dev/null
+
+echo "getting .bashrc from arcolinux-root"
+wget https://raw.githubusercontent.com/arcolinux/arcolinux-root/master/root/.bashrc-latest -O ../work/archiso/airootfs/etc/skel/.bashrc
+
+echo
+echo "################################################################## "
 echo "Phase 3 : Renaming the iso to BYOI and the desktop"
 echo "################################################################## "
-echo "Let us change the name"
+echo
+echo "Renaming to ArcoLinuxB"-$desktop
 
-sed -i 's/'$oldname1'/'$newname1'/g' ../archiso/build.sh
-sed -i 's/'$oldname2'/'$newname2'/g' ../archiso/build.sh
-sed -i 's/'$oldname3'/'$newname3'/g' ../archiso/airootfs/etc/os-release
-sed -i 's/'$oldname4'/'$newname4'/g' ../archiso/airootfs/etc/os-release
-sed -i 's/'$oldname5'/'$newname5'/g' ../archiso/airootfs/etc/lsb-release
-sed -i 's/'$oldname6'/'$newname6'/g' ../archiso/airootfs/etc/lsb-release
+sed -i 's/'$oldname1'/'$newname1'/g' ../work/archiso/build.sh
+sed -i 's/'$oldname2'/'$newname2'/g' ../work/archiso/build.sh
+sed -i 's/'$oldname3'/'$newname3'/g' ../work/archiso/airootfs/etc/os-release
+sed -i 's/'$oldname4'/'$newname4'/g' ../work/archiso/airootfs/etc/os-release
+sed -i 's/'$oldname5'/'$newname5'/g' ../work/archiso/airootfs/etc/lsb-release
+sed -i 's/'$oldname6'/'$newname6'/g' ../work/archiso/airootfs/etc/lsb-release
 
+echo
+echo "################################################################## "
 echo "Phase 4 : Let us build the iso"
 echo "################################################################## "
-echo "Let us change the name"
+echo
 
 echo "Checking if archiso is installed"
 
@@ -139,7 +139,8 @@ fi
 
 echo "Copying files and folder to ~/arcolinuxb-build as root"
 sudo mkdir ~/arcolinuxb-build
-sudo cp -r /tmp/git-clone-arcolinux-iso/* ~/arcolinuxb-build
+sudo cp -r ../work/* ~/arcolinuxb-build
+
 
 cd ~/arcolinuxb-build/archiso
 
