f4ccf381d218619fd624a791c284c64618501e2a
Erik Dubois <erik.dubois@gmail.com>
2018-10-21 21:15:50 +0200

    more admin scripts

---
+++ installation-scripts/50-move-the-iso-to-documents.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+set -e
+##################################################################################################################
+# Author	:	Erik Dubois
+# Website	:	https://www.erikdubois.be
+# Website	:	https://www.arcolinux.info
+# Website	:	https://www.arcolinux.com
+# Website	:	https://www.arcolinuxd.com
+# Website	:	https://www.arcolinuxforum.com
+##################################################################################################################
+#
+#   DO NOT JUST RUN THIS. EXAMINE AND JUDGE. RUN AT YOUR OWN RISK.
+#
+##################################################################################################################
+
+[ -d ~/Documents/ArcoLinuxB ] || mkdir ~/Documents/ArcoLinuxB
+
+echo "moving iso to documents"
+cp ~/arcolinuxb-build/archiso/out/arcolinuxb* ~/Documents/ArcoLinuxB
