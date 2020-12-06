43e66b27e921289f572e11a25bb679e1c42ed0c8
Erik Dubois <erik.dubois@gmail.com>
2020-12-06 08:28:46 +0100

    version number change

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="plasma"
 	lightdmDesktop="plasma"
 
-	arcolinuxVersion='v20.11.9'
+	arcolinuxVersion='v21.01.2'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
