b3a0823cf955399840810f4527a0a41cc1bc7aa6
Erik Dubois <erik.dubois@gmail.com>
2020-12-16 12:57:50 +0100

    new version

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -29,7 +29,7 @@ echo
 	desktop="plasma"
 	lightdmDesktop="plasma"
 
-	arcolinuxVersion='v21.01.3'
+	arcolinuxVersion='v21.01.4'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
