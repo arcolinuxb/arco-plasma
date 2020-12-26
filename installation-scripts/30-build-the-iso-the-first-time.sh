428764530609b14a25be9e5aecb03c3c84ecfae5
Erik Dubois <erik.dubois@gmail.com>
2020-12-26 14:11:49 +0100

    new config for calamares

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="plasma"
 	lightdmDesktop="plasma"
 
-	arcolinuxVersion='v21.01.5'
+	arcolinuxVersion='v21.01.6'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
