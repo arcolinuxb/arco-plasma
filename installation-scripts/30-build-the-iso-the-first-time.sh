deac64f4483d0f5e79d8a23f70ae374ab7b0b842
Erik Dubois <erik.dubois@gmail.com>
2021-01-04 15:48:12 +0100

    version 21.01.8

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -29,7 +29,7 @@ echo
 	desktop="plasma"
 	lightdmDesktop="plasma"
 
-	arcolinuxVersion='v21.01.7'
+	arcolinuxVersion='v21.01.8'
 
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
