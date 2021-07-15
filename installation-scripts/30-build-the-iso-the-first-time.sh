9e96aa21d56750f3b07674f7cfeff8a5d96ac13e
Erik Dubois <erik.dubois@gmail.com>
2021-07-15 17:35:00 +0200

    archversion 56.1-1

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -34,7 +34,7 @@ echo
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 56-1"
+	archisoRequiredVersion="archiso 56.1-1"
 	buildFolder=$HOME"/arcolinuxb-build"
 	outFolder=$HOME"/ArcoLinuxB-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
