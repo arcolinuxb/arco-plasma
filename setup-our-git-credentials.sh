a92f0008eb9f4983b14e9a0720735116f699a10d
arcolinuxz <arcolinuxinfo@gmail.com>
2021-12-02 12:08:02 +0100

    setup

--- setup-our-git-credentials.sh
+++ setup-our-git-credentials.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 #
+#tput setaf 0 = black
+#tput setaf 1 = red
+#tput setaf 2 = green
+#tput setaf 3 = yellow
+#tput setaf 4 = dark blue
+#tput setaf 5 = purple
+#tput setaf 6 = cyan
+#tput setaf 7 = gray
+#tput setaf 8 = light blue
 ##################################################################################################################
 # Written to be used on 64 bits computers
 # Author 	: 	Erik Dubois
