985951885b57eccfcadf21f6ad1e373fd1d49eba
arcolinuxz <arcolinuxinfo@gmail.com>
2022-11-07 16:02:41 +0100

    archiso 68

--- installation-scripts/40-build-the-iso-local-again.sh
+++ installation-scripts/40-build-the-iso-local-again.sh
@@ -34,7 +34,7 @@ echo
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 67-1"
+	archisoRequiredVersion="archiso 68-1"
 	buildFolder=$HOME"/arcolinuxb-build"
 	outFolder=$HOME"/ArcoLinuxB-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
