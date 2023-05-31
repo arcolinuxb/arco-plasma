ddcd2cb5ea9f6604fc84d0686b694b536a151f95
arcolinuxz <arcolinuxinfo@gmail.com>
2023-05-31 12:53:58 +0200

    archiso 71

--- installation-scripts/30-build-the-iso-the-first-time.sh
+++ installation-scripts/30-build-the-iso-the-first-time.sh
@@ -34,7 +34,7 @@ echo
 	isoLabel='arcolinuxb-'$desktop'-'$arcolinuxVersion'-x86_64.iso'
 
 	# setting of the general parameters
-	archisoRequiredVersion="archiso 70-1"
+	archisoRequiredVersion="archiso 71-1"
 	buildFolder=$HOME"/arcolinuxb-build"
 	outFolder=$HOME"/ArcoLinuxB-Out"
 	archisoVersion=$(sudo pacman -Q archiso)
